361b0a01c6c48c73145583596d7e824109b3b641
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-07-19 19:45:58 +0100

    setup for ai project

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <Button>click me</Button>
+    </div>
+  );
+};
+export default DashboardPage;
