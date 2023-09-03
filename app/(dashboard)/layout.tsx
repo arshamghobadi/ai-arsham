88176d73ee466b5551c595e757adc3a020824469
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-09-03 09:35:31 +0100

    sidbar basic ui

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,15 @@
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full relative">
+      <div className="hidden h-full md:w-72 md:flex md:flex-col md:fixed md:inset-y-0 z-30 bg-gray-900">
+        <div>heloo sidbar</div>
+      </div>
+      <main className="md:pl-72">
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default DashboardLayout;
